9792bdd95e6e5eb22a115daaa494e39c6aa356f6
John Irungu <johnirungu977@gmail.com>
2024-11-11 10:26:53 +0300

    JWT Token Error

--- Backend/middlewares/auth.js
+++ Backend/middlewares/auth.js
@@ -1,23 +1,27 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 import { User } from "../models/user.js";
+import { config } from "dotenv";
 
+config({
+  path: "../.env",
+});
 
 export const isAuthenticated = async (req, res, next) => {
-    try {
-        const { token } = req.cookies;
-        if (!token) {
-            res.status(404).json({
-                success: false,
-                message: "Login First"
-            })
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded._id);
-        next();
-    } catch (error) {
-        res.status(404).json({
-            success: false,
-            error,
-        })
+  try {
+    const { token } = req.cookies;
+    if (!token) {
+      res.status(404).json({
+        success: false,
+        message: "Login First",
+      });
     }
-}
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded._id);
+    next();
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      error,
+    });
+  }
+};
